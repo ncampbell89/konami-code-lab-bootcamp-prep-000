90ba3513faf1cdc4fc81308102b172a753b66aab
Nicole Campbell <nscampbell303@gmail.com>
2017-10-13 21:33:42 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -1,27 +1,24 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
-let index = 0
-
-document.body.addEventListener('keydown', function(e) {
-  if (e.which === code) {
-    alert("Hurray!")
-  }
-})
-
 function init() {
-  let key = parseInt(e.detail || e.which);
+  let index = 0
 
-  if (key === code[index]) {
-    index++;
+  document.body.addEventListener('keydown', onKeyDownHandler)
 
-    if (index === code.length) {
-      alert("Hurray!");
+  function onKeyDownHandler(e) {
 
+    let key = parseInt(e.detail || e.which);
+
+    if (key === code[index]) {
+      index++;
+
+      if (index === code.length) {
+        alert("Hurray!");
+        index = 0;
+      }
+
+    } else {
       index = 0;
     }
-
-  } else {
-    index = 0;
   }
-
 }
