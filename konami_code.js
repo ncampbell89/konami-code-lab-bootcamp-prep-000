9f9319ad3af6460e1db9da4139c83fbddde13c02
Nicole Campbell <nscampbell303@gmail.com>
2017-10-13 20:42:15 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -2,7 +2,7 @@ const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
 let index = 0
 
-function init(e) {
+function init() {
   let key = parseInt(e.detail || e.which);
 
   if (key === code[index]) {
